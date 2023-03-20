066c7e1037328ca987a73b8d47b7256514d16b3d
Rafael Andrade <rafaelrki123@gmail.com>
2023-03-20 14:54:32 -0300

    Criei/Pratiquei novos exercícios de JS (if/else)

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 50
+console.log(`A velocidade é ${vel}`)
+if (vel > 60) {
+    console.log(`Você ultrapassou o limite da via. Multado.`)
+}
+
+console.log(`Use o cinto de segurança e respeite a sinalização da via.`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Austrália'
+if (pais == 'Brasil') {
+    console.log(`Você é Brasileiro!`)
+}
+else {
+    console.log(`O seu país é: ${pais}, portanto, é estrangeiro.`)
+}
